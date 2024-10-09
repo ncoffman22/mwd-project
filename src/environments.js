648895dc3f1deeacb2298dc4c877237b5687ce12
ncoffman22 <123186855+ncoffman22@users.noreply.github.com>
2024-10-09 18:10:37 -0400

    Tying to back4app and packages

---
+++ src/environments.js
@@ -0,0 +1,5 @@
+modules.exports = {
+    APPLICATION_ID: "aQlVwbl51i8tudLZ2T53nXJHzvlpgT9IluGZoKOd",
+    JAVASCRIPT_KEY: "Lhp38Z7N6mmf0zUrL4zRXFBLxI4AhTAysy3x5KdG",
+    PARSE_URL: "https://parseapi.back4app.com/"
+}
