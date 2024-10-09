afaee99de08e304ad9f922ea3addbc3f4c14d06a
ncoffman22 <123186855+ncoffman22@users.noreply.github.com>
2024-10-09 16:54:52 -0500

    Delete Desktop/ModernWebDev/my-app directory

--- Desktop/ModernWebDev/my-app/src/environments.js
+++
@@ -1,6 +0,0 @@
-modules.exports = {
-    APPLICATION_ID: "aQlVwbl51i8tudLZ2T53nXJHzvlpgT9IluGZoKOd",
-    JAVASCRIPT_KEY: "Lhp38Z7N6mmf0zUrL4zRXFBLxI4AhTAysy3x5KdG",
-    SERVER_URL: "https://parseapi.back4app.com/"
-}
-
